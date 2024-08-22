d098dced82a7a6ad350828177d73fc87a4e6a9b6
Maicon Friedel <maicon.friedel@grupokyly.com.br>
2024-08-22 14:50:49 -0300

    feat: add i18n for english and portugues

---
+++ src/i18n/config.ts
@@ -0,0 +1,21 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import en from './en/translation.json';
+import pt from './pt/translation.json';
+
+i18n.use(LanguageDetector).use(initReactI18next).init({
+  fallbackLng: 'en',
+  supportedLngs: ['en', 'pt'],
+  // debug: true,
+  resources: {
+    en: { translation: en },
+    pt: { translation: pt },
+  },
+  interpolation: {
+    escapeValue: false // react already safes from xss
+  },
+
+});
+
+export default i18n;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+import "./i18n/config";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
+  </StrictMode>
+);
