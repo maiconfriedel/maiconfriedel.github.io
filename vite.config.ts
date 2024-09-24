842e38a5aa0ce2fa339e2d4f34774575a4e70d17
Maicon Friedel <maicon.friedel@grupokyly.com.br>
2024-09-24 16:09:27 -0300

    fix: added 404 page to redirect to the page

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 
@@ -9,4 +10,12 @@ export default defineConfig({
   plugins: [
     TanStackRouterVite(),
     react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        404: path.resolve(__dirname, "public/404.html"),
+      },
+    },
+  },
 })
